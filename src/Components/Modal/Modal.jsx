a0964a07f343f5f1c8d06b8803d98996a840594f
Maicol <72515186+maicol240@users.noreply.github.com>
2024-10-17 00:55:23 -0400

    Add form submition

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,13 +1,39 @@
 import "./Modal.css";
-
+import { useRef } from "react";
 export default function Modal({ showModal, onClose }) {
   const formInput = ["Name", "Cover", "Author", "Link"];
+  statusRef = useRef(null);
+  const inputRefs = formInput.reduce((acc, input) => {
+    acc[input] = useRef(null);
+    return acc;
+  }, {});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let img = inputRefs.Cover.current.value;
+    if (Img === null || img === "") {
+      img = "https://via.placeholder.com/150";
+    }
+    let link = inputRefs.Link.current.value;
+
+    if (link === null || link === "") {
+      link = "";
+    }
+    const book = {
+      img: img,
+      title: inputRefs.Name.current.value,
+      author: inputRefs.Author.current.value,
+      status: statusRef.current.value,
+      link: link,
+    };
+  };
 
   if (showModal)
     return (
       <>
         <div className="overlay">
-          <form className="modal">
+          <form onSubmit={handleSubmit} className="modal">
             <h1 style={{ textDecoration: "underline" }}>Book</h1>
             <div className="form-group">
               {formInput.map((input, index) => (
@@ -19,6 +45,7 @@ export default function Modal({ showModal, onClose }) {
                     type={input === "Link" ? "url" : "text"} // Use type "url" for Link
                     id={input}
                     name={input}
+                    ref={inputRefs[input]}
                   />
                 </div>
               ))}
